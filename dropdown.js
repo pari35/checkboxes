2188c3c6457afa132917791023453bdfa1ffd636
Paritosh Pardeshi <Paritoshpardeshi35@gmail.com>
2022-02-02 20:29:21 +0530

    stations tab added

--- dropdown.js
+++ dropdown.js
@@ -5,6 +5,14 @@ const pcb = document.getElementById("chkboxPan")
 const states = [mcb, gocb, gcb, pcb]
 const codeNames = ["maha", "goa", "guj", "pan"]
 const areacodenames=["pune","satara","kolhapur","fort"]
+const stationcodes=["katraj","camp","pimpri","phaltan"]
+// const stationcodesfromdb=[]
+// fetch("GET", url, (e) =>{
+//     // fo .....
+//     // e.data.forEach(d) ... {
+//     //     stationcodesfromdb.push(d)
+//     // }
+// })
 
 let checkedArray = {
     "maha": false,
@@ -20,7 +28,18 @@ let checkedArrayArea = {
 }
 
 
+let stations={
+    "katraj":false,
+    "camp":false,
+    "pimpri":false,
+    "phaltan":false,
+}
 
+let assignstation={
+    "katraj":["FS101","FS102","FS103"],
+    "camp":["CM101","CM102","CM103"],
+    "pimpri":["P101,P102,P103"]
+}
 // let arar = []
 // console.log("hello");
 // console.log(checkedArray["maha"]);
@@ -91,7 +110,7 @@ const populate = (t) => {
                     // console.log("areael : ", areaelement);
                     areaname += `
                     <div class="form-check">
-                    <input class="form-check-input" )" id="chkboxMaha" type="checkbox" value="" id="flexCheckDefault">
+                    <input class="form-check-input" )" id="chkboxMaha" type="checkbox" value="" onchange="addstation('${regionarea}')" id="flexCheckDefault">
                     <label class="form-check-label" for="flexCheckDefault">
                     ${regionarea}
                     </label>
@@ -102,7 +121,32 @@ const populate = (t) => {
         }
     }
     document.getElementById('area').innerHTML=areaname
+
 }
+
+function addstation(statin) {
+    stations[statin]= !stations[statin]
+    let stt =` `
+    for (let stationcod of stationcodes){
+        console.log("first loop stationcod:",stationcod);
+        console.log("sec loop stations[stationcod]:",stations[stationcod]);
+        if(stations[stationcod]==true){
+
+            for(let asst of assignstation[stationcod]){
+                // for(let as of asst){
+                    stt+=` <div class="form-check">
+                    <input class="form-check-input" )" id="chkboxMaha" type="checkbox"  id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                    ${asst}
+                    </label>
+                    </div> `
+                // }
+            }
+        }
+    }      document.getElementById('stations').innerHTML=stt;
+
+} 
+
 
 // const populate = (dist) => {
 //     arar.push()
